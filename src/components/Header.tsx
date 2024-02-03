eb7137fe4b9fdd248974c417866f04a5e5c4a4d0
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-03 03:44:02 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,24 @@
 const Header = () => {
   return (
     <>
-      <main className="flex justify-end text-[18px] gap-5">
-        <button>
-            <p>GitHub</p>
-        </button>
-        <button>
-            <p>LinkedIn</p>
-        </button>
-        <button>
-            <p>Discord</p>
-        </button>
+      <main className="flex justify-between text-[18px] gap-3 sticky">
+        <div className="font-[700] cursor-pointer">
+          <p className="py-[0.4em] px-[1em]">Tiba.dev</p>
+        </div>
+        <div className="">
+          <button>
+            <p>Home</p>
+          </button>
+          <button>
+            <p>About</p>
+          </button>
+          <button>
+            <p>Projects</p>
+          </button>
+          <button>
+            <p>Contacts</p>
+          </button>
+        </div>
       </main>
     </>
   );
