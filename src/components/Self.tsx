c1abfdfbae1533f6455e0c1fa977470e64549791
AskTiba <132336450+AskTiba@users.noreply.github.com>
2024-02-03 03:43:40 +0300

    Update Self.tsx

--- src/components/Self.tsx
+++ src/components/Self.tsx
@@ -1,31 +1,30 @@
+import img from "../assets/img5.jpg";
+
 const Self = () => {
   return (
     <>
-      <main className="ml-[400px]">
-        <div className="flex flex-col">
-          <div className="flex justify-start text-[30px] mb-10">
-            <p>
-              I’ve spent a couple of decades creating brands <br /> and digital
-              products and obsessing over what <br /> makes them fly. I have a
-              creative studio called <br /> The Workshop and invest in
-              early-stage ventures <br /> — collaborating on brave ideas with
-              amazing <br /> people.
+      <main className="mb-20">
+        <div className="flex gap-10 items-center justify-center">
+          <img src={img} alt="" className="h-[400px] rounded-md object-cover" />
+          <div className="">
+            <p className="font-[900] text-[#32cd32] text-[20px]">ABOUT ME</p>
+            <p className="font-[700] text-[18px] mt-2">
+              {" "}
+              A passionate front-end React Developer based in Kamapala, Uganda.
+              📍
             </p>
-          </div>
-          <div className="flex justify-start text-[48px] mb-10 font-semibold">
-            <p>
-              I partner with venture <br /> capital and accelerators <br /> —
-              helping shape <br />
-              investments as a Design <br /> Director.
-            </p>
-          </div>
-          <div className="flex justify-start text-[30px] mb-10">
-            <p>
-              I focus on early-stage design — taking a brand <br /> from zero to
-              one. My work helps companies make <br /> strategic decisions in
-              their foundational stages <br /> and leverage their position in
-              the market. Over the <br /> past few years, I’ve helped build four
-              startups and <br /> my work has led to over $55M in investment.
+            <p className="text-[17px] mt-3">
+              Hi, I’m Anthony with a background in Computer Networking and a
+              penchant for clean code, I specialize in crafting delightful user
+              experiences. My toolkit includes HTML, CSS (with a flair for
+              responsive design), JavaScript (ES6+), TypeScript, React, and
+              React Native. I’m also a fan of the utility-first approach with
+              Tailwind CSS. I’ve had the privilege of working on diverse
+              projects. From e-commerce websites that adapt seamlessly across
+              devices to interactive web apps that engage users, I thrive on
+              turning ideas into reality. One of my proudest achievements was
+              optimizing a landing page, resulting in a 30% increase in
+              conversion rates.
             </p>
           </div>
         </div>
